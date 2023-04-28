83879db35f7b22f024c216cc77defb02cee324c8
KaiCaire <125651900+KaiCaire@users.noreply.github.com>
2023-04-28 18:34:15 +0200

    Adjusted music & FX volumes + Added Wolf Modules
    Used Audacity to do it so that previous code attempting to manually lower the volume from the project could be removed
    
    Also added Wolf (inheriting from Enemy) Modules

---
+++ AlteredBeast Proto_/Project_4_Handout/Source/Wolf.h
@@ -0,0 +1,39 @@
+#ifndef __WOLF_H__
+#define __WOLF_H__
+
+#include "Enemy.h"
+#include "Path.h"
+
+class Zombie : public Enemy {
+public:
+	Zombie(int x, int y);
+
+	void Update() override;
+
+	void OnCollision(Collider* collider) override;
+
+	bool alive = true;
+	bool destroyed = false;
+
+	enum class Direction {
+		LEFT,
+		RIGHT
+	};
+
+	int hp = 10;
+
+	uint destroyedCountdown = 50;
+
+
+private:
+
+	Direction dir;
+	Animation walkAnim;
+	Animation deathAnim;
+
+
+};
+
+
+
+#endif
