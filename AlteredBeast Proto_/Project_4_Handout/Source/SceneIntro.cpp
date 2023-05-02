e93e80bfe88654a7a805445422cc7b0ece488d3f
KaiCaire <125651900+KaiCaire@users.noreply.github.com>
2023-05-02 18:12:22 +0200

    Screen Intro Half Implemented!!!
    * happykainoises *
    :warning: the animations may run really slowly for you, since mine accelerates all execution, so if you could check how fast it actually runs pls

--- AlteredBeast Proto_/Project_4_Handout/Source/SceneIntro.cpp
+++ AlteredBeast Proto_/Project_4_Handout/Source/SceneIntro.cpp
@@ -9,23 +9,60 @@
 #include "SDL/include/SDL.h"
 #include "ModulePlayer.h"
 
+//video reference: https://www.youtube.com/watch?v=NVrWoPUSW6E&ab_channel=ArcadeGameClips (first 7 seconds)
+
 SceneIntro::SceneIntro(bool startEnabled) : Module(startEnabled)
 {
+	mural = { 0,0,320,224 };
+	blueScreen = { 321,0,320,224 };
 	whiteLetters = { 321,225,320,224 };
 	redLetters = { 642,225,320,224 };
 	blueLetters = { 0,225,320,224 };
+	SEGA = {3, 451, 77, 24};
+
+	muralFade.PushBack({ 1,1,320,224 });
+	muralFade.PushBack({ 322,1,320,224 });
+	muralFade.PushBack({ 643,1,320,224 });
+	muralFade.PushBack({ 964,1,320,224 });
+	muralFade.PushBack({ 1285,1,320,224 });
+	muralFade.PushBack({ 1,226,320,224 });
+	muralFade.PushBack({ 322,226,320,224 });
+	muralFade.PushBack({ 643,226,320,224 });
+	muralFade.PushBack({ 964,226,320,224 });
+	muralFade.PushBack({ 1285,226,320,224 });
+	muralFade.totalFrames = 10;
+	muralFade.loop = false;
+	muralFade.speed = 0.05f;
+
+	titleFade.PushBack({ 1,1,320,224 });
+	titleFade.PushBack({ 322,1,320,224 });
+	titleFade.PushBack({ 643,1,320,224 });
+	titleFade.PushBack({ 964,1,320,224 });
+	titleFade.PushBack({ 1285,1,320,224 });
+	titleFade.PushBack({ 1,226,320,224 });
+	titleFade.PushBack({ 322,226,320,224 });
+	titleFade.PushBack({ 643,226,320,224 });
+	titleFade.PushBack({ 964,226,320,224 });
+	titleFade.PushBack({ 1285,226,320,224 });
+	titleFade.totalFrames = 10;
+	titleFade.loop = false;
+	titleFade.speed = 0.05f;
 
 	letterFlash.PushBack(whiteLetters);
 	letterFlash.PushBack(redLetters);
 	letterFlash.PushBack(whiteLetters);
 	letterFlash.PushBack(redLetters);
-	letterFlash.totalFrames = 4;
-	letterFlash.speed = 0.05f;
+	/*letterFlash.PushBack(redTitle);*/
+	letterFlash.totalFrames = 5;
+	letterFlash.speed = 0.1f;
 	letterFlash.loop = false;
 }
 
 SceneIntro::~SceneIntro()
 {
+	App->textures->Unload(assetsTex);
+	App->textures->Unload(blueTitleTex);
+	App->textures->Unload(muralFadeTex);
 
 }
 
@@ -35,9 +72,9 @@ bool SceneIntro::Start()
 	LOG("Loading background assets");
 
 	bool ret = true;
-
-	bgTexture = App->textures->Load("Assets/Intro/introScreen6.png");
-	lettersTex = App->textures->Load("Assets/Intro/introScreenUnited.png");
+	blueTitleTex = App->textures->Load("Assets/Intro/blueTitleUnited.png");
+	muralFadeTex = App->textures->Load("Assets/Intro/muralScreenUnited.png");
+	assetsTex = App->textures->Load("Assets/Intro/introScreenUnited.png");
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
@@ -48,15 +85,36 @@ bool SceneIntro::Start()
 
 update_status SceneIntro::Update()
 {
+	changeCountdown--;
+	if (changeCountdown <= 0) { screenChange1 = true; }
+
+	if (screenChange1){
+
+		currentTex = &muralFadeTex;
+		currentAnim = &muralFade;
+		
+		if (muralFade.HasFinished()) {
+			currentTex = &blueTitleTex;
+			currentAnim = &titleFade;
+			
+
+		}
+		currentAnim->Update();
+	}
+
+	
+	
+	
+
 	if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
 	{
 		/*App->player->Enable();*/
 		App->fade->FadeToBlack(this, (Module*)App->scene, 60.0f);
 		
 	}
-	else {
-		currentAnim = &letterFlash;
-	}
+	
+	
+	
 	return update_status::UPDATE_CONTINUE;
 }
 
@@ -64,7 +122,13 @@ update_status SceneIntro::Update()
 update_status SceneIntro::PostUpdate()
 {
 	// Draw everything --------------------------------------
-	App->render->Blit(lettersTex, 0, 0, &currentAnim->GetCurrentFrame());
+	if (!screenChange1) {
+		App->render->Blit(assetsTex, 0, 0, &mural);
+	} else {
+		App->render->Blit(*currentTex, 0, 0, &currentAnim->GetCurrentFrame());
+
+	}
+	
 
 	return update_status::UPDATE_CONTINUE;
 }
