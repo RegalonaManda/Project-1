dc7dbe75d88fcdcf2590eac25aba893255658a4e
Nil-BT <nilbarbara00@gmail.com>
2023-05-22 11:16:58 +0200

    No more crash

--- AlteredBeast Proto_/Project_4_Handout/Source/ModuleScene.cpp
+++ AlteredBeast Proto_/Project_4_Handout/Source/ModuleScene.cpp
@@ -117,7 +117,7 @@ bool ModuleScene::Start()
 	App->enemies->AddEnemy(ENEMY_TYPE::ZOMBIE, 350, 200, true);
 	App->enemies->AddEnemy(ENEMY_TYPE::ZOMBIE, 525, 200,false);
 	App->enemies->AddEnemy(ENEMY_TYPE::ZOMBIE, 550, 200, true);
-	App->enemies->AddEnemy(ENEMY_TYPE::SKULL, 600, 200, false);
+	//App->enemies->AddEnemy(ENEMY_TYPE::SKULL, 600, 200, false);
 	
 
 
@@ -141,7 +141,7 @@ bool ModuleScene::Start()
 	App->enemies->AddEnemy(ENEMY_TYPE::BROWNWOLF, 1100, 140,false);
 	App->enemies->AddEnemy(ENEMY_TYPE::WHITEWOLF, 1105, 140, true);
 	App->enemies->AddEnemy(ENEMY_TYPE::NEFF, 1161 + 250, 110,false);
-	App->enemies->AddEnemy(ENEMY_TYPE::SKULL, 1400, 200, false);
+	//App->enemies->AddEnemy(ENEMY_TYPE::SKULL, 1400, 200, false);
 	
 	
 	
